410a13b7fdd37003ba684ff8133dce33b8dae757
Dragas <github@dragas.xyz>
2023-09-18 01:47:49 +0300

    adds support to read config file, and to manage weight, and whether experiment is running

--- infrastructure-task/infrastructure/viewer-request-lambda/index.js
+++ infrastructure-task/infrastructure/viewer-request-lambda/index.js
@@ -1,12 +1,42 @@
 // The structure of events: https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/lambda-event-structure.html#example-viewer-request
-function handler(event, context, callback) {
+
+const awsS3Library = require("@aws-sdk/client-s3");
+const s3 = new awsS3Library.S3Client({region: "us-east-1"});
+const configReference = new awsS3Library.GetObjectCommand({
+    Bucket: 'fft-assignment-files-831746801341', // FIXME: hardcoded generated name. would need a templating engine like moustache. out of scope for assignment
+    Key: 'config/experiments/home.json',
+});
+
+
+async function handler(event, context, callback) {
   const request = event.Records[0].cf.request;
   const originalUri = request.uri;
-  console.log("Handing request on", originalUri);
-  console.log("Headers are", request.headers);
+  const configurationResponse = await s3.send(configReference);
+  const configurationBody = await configurationResponse.Body.transformToString("utf-8");
+  const configuration = JSON.parse(configurationBody);
   const lastPart = originalUri.substring(originalUri.lastIndexOf("/") + 1);
-  if(lastPart === "home") {
+  const isEnabled = configuration["EXPERIMENT_ENABLED"];
+  if(isEnabled && lastPart.indexOf("home") === 0) {
+    if(lastPart.indexOf("home/variant") === 0) {
+      // user is trying to access experimental pages directly, return a redirect
+      const redirectResponse = {
+        status: '307', // must return 307 Moved Temporarily because busting 301 Moved Permanently is painful
+        statusDescription: 'Moved Temporarily',
+        headers: {
+          'location': [{
+            key: 'Location',
+            value: '/home',
+          }],
+          'cache-control': [{
+            key: 'Cache-Control',
+            value: "no-cache"
+          }],
+        },
+      };
+      return callback(null, redirectResponse);
+    }
     let cookies = request.headers.cookie;
+    const weight = configuration["TRAFFIC_WEIGHT"];
     const primaryCookie = "Experiment=A";
     const secondaryCookie = "Experiment=B";
     const experimentMap = {
@@ -37,7 +67,7 @@ function handler(event, context, callback) {
       // user isnt in AB testing yet so give variant 1 90% of the time
       // or user blocks cookies.
       // might want to use a more consistent mechanism here but its fine for now
-      if(Math.random() <= 0.9) {
+      if(Math.random() <= weight) {
         experimentCookieValue = primaryCookie;
       }
       else {
--- infrastructure-task/infrastructure/viewer-response-lambda/index.js
+++ infrastructure-task/infrastructure/viewer-response-lambda/index.js
@@ -1,8 +1,35 @@
-function handler(event, context, callback) {
+const awsS3Library = require("@aws-sdk/client-s3");
+const s3 = new awsS3Library.S3Client({region: "us-east-1"});
+const configReference = new awsS3Library.GetObjectCommand({
+    Bucket: 'fft-assignment-files-831746801341', // FIXME: hardcoded generated name. would need a templating engine like moustache. out of scope for assignment
+    Key: 'config/experiments/home.json',
+});
+
+async function handler(event, context, callback) {
     const request = event.Records[0].cf.request;
     const response = event.Records[0].cf.response;
-    console.log(request.uri);
-    callback(null, response);
+    const configurationResponse = await s3.send(configReference);
+    const configurationBody = await configurationResponse.Body.transformToString("utf-8");
+    const configuration = JSON.parse(configurationBody);
+    if(configuration["EXPERIMENT_ENABLED"]) {
+        const primaryCookie = "Experiment=A";
+        const secondaryCookie = "Experiment=B";
+        const cookieMap = {
+            "/home/variant-1.html": primaryCookie,
+            "/home/variant-2.html": secondaryCookie
+        }
+        const chosenCookie = cookieMap[request.uri];
+        // users may switch experiments daily
+        if(chosenCookie !== undefined) {
+            response.headers["set-cookie"] = [
+                {
+                  "key": "Set-Cookie",
+                  "value": `${chosenCookie}; Max-Age=86400; SameSite=Strict`
+                }
+            ];
+        }
+    }
+    return callback(null, response);
 }
 
 module.exports = {
